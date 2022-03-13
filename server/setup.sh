63f20c27c9351f1f30e6b639664e314eab1e67c5
sidney.verite <sidney.verite@turbinekreuzberg.com>
2022-03-13 17:24:52 +0100

    test

--- server/setup.sh
+++ server/setup.sh
@@ -4,11 +4,9 @@ set -eoux pipefail
 #adduser sidney sudo
 #adduser luwey sudo
 
-apt-get install apache2 zsh curl wget git
-#apt-get install expect
+apt-get install curl wget git 
 
 
-#add a way to link the github to server
+#activate plesk
 
-## oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
